bddc41a548f08ef04a97f6d1e149f3b703b99643
Thomas Chamberlin <tchamber@nrao.edu>
2021-06-10 14:59:42 -0400

    Prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import { createForm } from 'felte';
-  import reporter from '@felte/reporter-dom';
-  import { validator } from '@felte/validator-yup';
-  import * as yup from 'yup';
+  import { createForm } from "felte";
+  import reporter from "@felte/reporter-dom";
+  import { validator } from "@felte/validator-yup";
+  import * as yup from "yup";
 
   const schema = yup.object({
     conf_name: yup.string().required(),
@@ -10,7 +10,11 @@
     nwin: yup.number().typeError("Must be a positive number").positive(),
     rest_freq: yup.number().typeError("Must be a positive number").positive(),
     bandwidth: yup.number().typeError("Must be a positive number").positive(),
-    nchan: yup.number().typeError("Must be a positive integer").positive().integer(),
+    nchan: yup
+      .number()
+      .typeError("Must be a positive integer")
+      .positive()
+      .integer(),
     tint: yup.number().typeError("Must be a positive number").positive(),
     vframe: yup.string().required(),
     framevdef: yup.string().required(),
@@ -21,22 +25,22 @@
   });
 
   const receivers = [
-    {value: "Rcvr_340", label: "P-band"},
-    {value: "Rcvr_800", label: "800 MHz"},
-    {value: "Rcvr_1070", label: "PF2_1070"},
-    {value: "Rcvr1_2", label: "L-band"},
-    {value: "Rcvr2_3", label: "S-band"},
-    {value: "Rcvr4_6", label: "C-band"},
-    {value: "Rcvr8_10", label: "X-band"},
-    {value: "Rcvr12_18", label: "Ku"},
-    {value: "RcvrArray18_26", label: "KFPA"},
-    {value: "Rcvr26_40", label: "Ka"},
-    {value: "Rcvr40_52", label: "Q-band"},
-    {value: "Rcvr68_92", label: "W-band"},
-  ]
+    { value: "Rcvr_340", label: "P-band" },
+    { value: "Rcvr_800", label: "800 MHz" },
+    { value: "Rcvr_1070", label: "PF2_1070" },
+    { value: "Rcvr1_2", label: "L-band" },
+    { value: "Rcvr2_3", label: "S-band" },
+    { value: "Rcvr4_6", label: "C-band" },
+    { value: "Rcvr8_10", label: "X-band" },
+    { value: "Rcvr12_18", label: "Ku" },
+    { value: "RcvrArray18_26", label: "KFPA" },
+    { value: "Rcvr26_40", label: "Ka" },
+    { value: "Rcvr40_52", label: "Q-band" },
+    { value: "Rcvr68_92", label: "W-band" },
+  ];
   const { form, isValid, data, errors, setFields, reset } = createForm({
     onSubmit: (data) => console.log("Submit", data),
-    onError: error => error,
+    onError: (error) => error,
     extend: [validator, reporter({ single: true })],
     validateSchema: schema,
     initialValues: {
@@ -53,7 +57,7 @@
       noisecal: "",
       pol: "",
       xcor: false,
-    }
+    },
   });
 
   function genDummyData() {
@@ -70,7 +74,7 @@
       use_cal: false,
       noisecal: "high",
       pol: "linear",
-      xcor: true
+      xcor: true,
     });
     // TODO: Is there a away to avoid doing this manually?
     let element = document.getElementById("rx-select");
@@ -78,8 +82,7 @@
   }
 
   function genConfigString(data) {
-    return (
-      `Configure("""
+    return `Configure("""
   receiver="${data.rx}",
   obstype="Spectroscopy",
   backend="VEGAS",
@@ -99,8 +102,7 @@
   pol="${data.pol}",
   xcor=${data.xcor ? "True" : "False"},
   notchfilter="In",
-)"""`
-    )
+)"""`;
   }
 </script>
 
@@ -111,156 +113,298 @@
     <div class="col-lg-6">
       <form use:form>
         <div class="row mb-1">
-          <label class="col-sm-6 col-form-label" for="conf_name">Name of the Config section</label>
+          <label class="col-sm-6 col-form-label" for="conf_name"
+            >Name of the Config section</label
+          >
           <div class="col-sm-6">
-            <input class="form-control" type="text" name="conf_name">
-            <div class="error-msg" id="conf_name-validation" data-felte-reporter-dom-for="conf_name" />
+            <input class="form-control" type="text" name="conf_name" />
+            <div
+              class="error-msg"
+              id="conf_name-validation"
+              data-felte-reporter-dom-for="conf_name"
+            />
           </div>
         </div>
-        
+
         <div class="row mb-1">
           <label class="col-form-label col-sm-6 pt-0" for="rx">Receiver</label>
           <div class="col-sm-6">
-            <select class="form-select" id="rx-select" name="rx" aria-label="Select receiver">
-                <option value="">Select Receiver</option>
-                {#each receivers as receiver}
-                <option value="{receiver.value}">{receiver.label}</option>
+            <select
+              class="form-select"
+              id="rx-select"
+              name="rx"
+              aria-label="Select receiver"
+            >
+              <option value="">Select Receiver</option>
+              {#each receivers as receiver}
+                <option value={receiver.value}>{receiver.label}</option>
               {/each}
             </select>
-            <div class="error-msg" id="rx-validation" data-felte-reporter-dom-for="rx" />
+            <div
+              class="error-msg"
+              id="rx-validation"
+              data-felte-reporter-dom-for="rx"
+            />
           </div>
         </div>
-        
+
         <div class="row mb-1">
-          <label class="col-sm-6 col-form-label" for="nwin"># of spectral windows</label>
+          <label class="col-sm-6 col-form-label" for="nwin"
+            ># of spectral windows</label
+          >
           <div class="col-sm-6">
-            <input class="form-control" type="text" inputmode="numeric" name="nwin">
-            <div class="error-msg" id="nwin-validation" data-felte-reporter-dom-for="nwin" />
+            <input
+              class="form-control"
+              type="text"
+              inputmode="numeric"
+              name="nwin"
+            />
+            <div
+              class="error-msg"
+              id="nwin-validation"
+              data-felte-reporter-dom-for="nwin"
+            />
           </div>
         </div>
-    
+
         <div class="row mb-1">
-          <label class="col-sm-6 col-form-label" for="rest_freq">Rest Frequency</label>
+          <label class="col-sm-6 col-form-label" for="rest_freq"
+            >Rest Frequency</label
+          >
           <div class="col-sm-6">
-            <input class="form-control" type="text" inputmode="numeric" name="rest_freq">
-            <div class="error-msg" id="rest_freq-validation" data-felte-reporter-dom-for="rest_freq" />
+            <input
+              class="form-control"
+              type="text"
+              inputmode="numeric"
+              name="rest_freq"
+            />
+            <div
+              class="error-msg"
+              id="rest_freq-validation"
+              data-felte-reporter-dom-for="rest_freq"
+            />
           </div>
         </div>
-        
+
         <div class="row mb-1">
-          <label class="col-sm-6 col-form-label" for="bandwidth">Bandwidth</label>
+          <label class="col-sm-6 col-form-label" for="bandwidth"
+            >Bandwidth</label
+          >
           <div class="col-sm-6">
-            <input class="form-control" type="text" inputmode="numeric" name="bandwidth">
-            <div class="error-msg" id="bandwidth-validation" data-felte-reporter-dom-for="bandwidth" />
-        </div>
-        </div>
-        
-        <div class="row mb-1">
-          <label class="col-sm-6 col-form-label" for="nchan"># of channels</label>
-          <div class="col-sm-6">
-            <input class="form-control" type="text" inputmode="numeric" name="nchan">
-            <div class="error-msg" id="nchan-validation" data-felte-reporter-dom-for="nchan" />
+            <input
+              class="form-control"
+              type="text"
+              inputmode="numeric"
+              name="bandwidth"
+            />
+            <div
+              class="error-msg"
+              id="bandwidth-validation"
+              data-felte-reporter-dom-for="bandwidth"
+            />
           </div>
         </div>
-    
+
         <div class="row mb-1">
-          <label class="col-sm-6 col-form-label" for="tint">Integration Time</label>
+          <label class="col-sm-6 col-form-label" for="nchan"
+            ># of channels</label
+          >
           <div class="col-sm-6">
-            <input class="form-control" type="text" inputmode="numeric" name="tint">
-            <div class="error-msg" id="tint-validation" data-felte-reporter-dom-for="tint" />
+            <input
+              class="form-control"
+              type="text"
+              inputmode="numeric"
+              name="nchan"
+            />
+            <div
+              class="error-msg"
+              id="nchan-validation"
+              data-felte-reporter-dom-for="nchan"
+            />
+          </div>
+        </div>
+
+        <div class="row mb-1">
+          <label class="col-sm-6 col-form-label" for="tint"
+            >Integration Time</label
+          >
+          <div class="col-sm-6">
+            <input
+              class="form-control"
+              type="text"
+              inputmode="numeric"
+              name="tint"
+            />
+            <div
+              class="error-msg"
+              id="tint-validation"
+              data-felte-reporter-dom-for="tint"
+            />
           </div>
         </div>
 
         <fieldset class="row mb-1">
-          <legend class="col-form-label col-sm-6 pt-0">Velocity reference</legend>
+          <legend class="col-form-label col-sm-6 pt-0"
+            >Velocity reference</legend
+          >
           <div class="col-sm-6">
-              <div class="form-check">
-              <input class="form-check-input" type="radio" name="vframe" id="vframe_lsrk" value="lsrk">
-              <label class="form-check-label" for="vframe_lsrk">
-                Lsrk
-              </label>
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="vframe"
+                id="vframe_lsrk"
+                value="lsrk"
+              />
+              <label class="form-check-label" for="vframe_lsrk"> Lsrk </label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="vframe" id="vframe_helio" value="helio">
-              <label class="form-check-label" for="vframe_helio">
-                Helio
-              </label>
+              <input
+                class="form-check-input"
+                type="radio"
+                name="vframe"
+                id="vframe_helio"
+                value="helio"
+              />
+              <label class="form-check-label" for="vframe_helio"> Helio </label>
             </div>
-            <div class="error-msg" id="vframe-validation" data-felte-reporter-dom-for="vframe" />
+            <div
+              class="error-msg"
+              id="vframe-validation"
+              data-felte-reporter-dom-for="vframe"
+            />
           </div>
         </fieldset>
-    
+
         <fieldset class="row mb-1">
-          <legend class="col-form-label col-sm-6 pt-0">Specify velocity frame for Doppler shift</legend>
+          <legend class="col-form-label col-sm-6 pt-0"
+            >Specify velocity frame for Doppler shift</legend
+          >
           <div class="col-sm-6">
-              <div class="form-check">
-              <input class="form-check-input" type="radio" name="framevdef" id="framevdef_lsrk" value="optical">
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="framevdef"
+                id="framevdef_lsrk"
+                value="optical"
+              />
               <label class="form-check-label" for="framevdef_optical">
                 Optical
               </label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="framevdef" id="framevdef_radio" value="radio">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="framevdef"
+                id="framevdef_radio"
+                value="radio"
+              />
               <label class="form-check-label" for="framevdef_radio">
                 Radio
               </label>
             </div>
-            <div class="error-msg" id="framevdef-validation" data-felte-reporter-dom-for="framevdef" />
+            <div
+              class="error-msg"
+              id="framevdef-validation"
+              data-felte-reporter-dom-for="framevdef"
+            />
           </div>
         </fieldset>
-    
+
         <div class="form-check form-switch">
-          <input class="form-check-input" type="checkbox" name="use_cal">
-          <label class="form-check-label" for="use_cal">Use the noise diode?</label>
+          <input class="form-check-input" type="checkbox" name="use_cal" />
+          <label class="form-check-label" for="use_cal"
+            >Use the noise diode?</label
+          >
         </div>
-      
+
         {#if $data.use_cal}
-        <fieldset class="row mb-1">
-          <legend class="col-form-label col-sm-6 pt-0">Strength of the noise diode</legend>
-          <div class="col-sm-6">
+          <fieldset class="row mb-1">
+            <legend class="col-form-label col-sm-6 pt-0"
+              >Strength of the noise diode</legend
+            >
+            <div class="col-sm-6">
               <div class="form-check">
-              <input class="form-check-input" type="radio" name="noisecal" id="noisecal_high" value="high">
-              <label class="form-check-label" for="noisecal_high">
-                High
-              </label>
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="noisecal"
+                  id="noisecal_high"
+                  value="high"
+                />
+                <label class="form-check-label" for="noisecal_high">
+                  High
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="noisecal"
+                  id="noisecal_low"
+                  value="low"
+                />
+                <label class="form-check-label" for="noisecal_low"> Low </label>
+              </div>
+              <div
+                class="error-msg"
+                id="noisecal-validation"
+                data-felte-reporter-dom-for="noisecal"
+              />
             </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="noisecal" id="noisecal_low" value="low">
-              <label class="form-check-label" for="noisecal_low">
-                Low
-              </label>
-            </div>
-            <div class="error-msg" id="noisecal-validation" data-felte-reporter-dom-for="noisecal" />
-          </div>
-        </fieldset>
+          </fieldset>
         {/if}
         <fieldset class="row mb-1">
-          <legend class="col-form-label col-sm-6 pt-0">Specify polarization state</legend>
+          <legend class="col-form-label col-sm-6 pt-0"
+            >Specify polarization state</legend
+          >
           <div class="col-sm-6">
-              <div class="form-check">
-              <input class="form-check-input" type="radio" name="pol" id="pol_linear" value="linear">
-              <label class="form-check-label" for="pol_linear">
-                Linear
-              </label>
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="pol"
+                id="pol_linear"
+                value="linear"
+              />
+              <label class="form-check-label" for="pol_linear"> Linear </label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="pol" id="pol_circular" value="circular">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="pol"
+                id="pol_circular"
+                value="circular"
+              />
               <label class="form-check-label" for="pol_circular">
                 Circular
               </label>
             </div>
-            <div class="error-msg" id="pol-validation" data-felte-reporter-dom-for="pol" />
+            <div
+              class="error-msg"
+              id="pol-validation"
+              data-felte-reporter-dom-for="pol"
+            />
           </div>
         </fieldset>
-        
+
         <div class="form-check form-switch">
-          <input class="form-check-input" type="checkbox" name="xcor">
-          <label class="form-check-label" for="xcor">Record Cross polarization as well?</label>
+          <input class="form-check-input" type="checkbox" name="xcor" />
+          <label class="form-check-label" for="xcor"
+            >Record Cross polarization as well?</label
+          >
         </div>
-        
+
         <button type="submit" class="btn btn-primary">Submit</button>
-        <button type="reset" class="btn btn-warning" on:click={reset}>Clear</button>
-        <button type="button" class="btn btn-info" on:click={genDummyData}>Insert Dummy Data</button>
+        <button type="reset" class="btn btn-warning" on:click={reset}
+          >Clear</button
+        >
+        <button type="button" class="btn btn-info" on:click={genDummyData}
+          >Insert Dummy Data</button
+        >
       </form>
     </div>
     <div class="col-lg-3">
@@ -268,7 +412,7 @@
         <h6>Form Data</h6>
         <code><pre>{JSON.stringify($data, null, 2)}</pre></code>
       </div>
-      
+
       <div class="row">
         <h6>Form Errors</h6>
         <code><pre>{JSON.stringify($errors, null, 2)}</pre></code>
@@ -280,18 +424,21 @@
         {#if $isValid}
           <code><pre>{genConfigString($data)}</pre></code>
         {:else}
-          <p on:click={genDummyData} style="cursor: pointer">Complete the form to generate a config string (or click here to use fake data)</p>
+          <p on:click={genDummyData} style="cursor: pointer">
+            Complete the form to generate a config string (or click here to use
+            fake data)
+          </p>
         {/if}
       </div>
     </div>
-  
+  </div>
 </main>
 
 <style>
   .error-msg {
     width: 100%;
-    margin-top: .25rem;
-    font-size: .875em;
+    margin-top: 0.25rem;
+    font-size: 0.875em;
     color: #dc3545;
   }
 </style>
